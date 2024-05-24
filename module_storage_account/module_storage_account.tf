2e0262fddf0fe6d733bf62b50825a550ce0321e7
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-24 19:07:48 -0400

    Update module_storage_account.tf

--- module_storage_account/module_storage_account.tf
+++ module_storage_account/module_storage_account.tf
@@ -1,27 +1,4 @@
 #module for storage account
-variable "storage_account_name" {
-  description = "The name of the storage account"
-}
-
-variable "resource_group_name" {
-  description = "The name of the storage account resource group"
-}
-
-variable "location" {
-  description = "The Azure region where the storage account will be created"
-  default     = "eastus"
-}
-
-variable "account_tier" {
-  description = "The storage account tier (Standard or Premium)"
-  default     = "Standard"
-}
-
-variable "account_replication_type" {
-  description = "The storage account replication type"
-  default     = "LRS"
-}
-
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
@@ -31,10 +8,3 @@ resource "azurerm_storage_account" "storage_account" {
 
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.storage_account.name
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.storage_account.id
-}
